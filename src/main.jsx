1f22c67c0199a00602146cb4d9092e2ab9a6c49c
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-12-27 16:11:36 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import LandingPage from "./pages/landingpage";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>
+);
---
+++ src/pages/landingPage.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function LandingPage() {
+  return (
+    <>
+      <div>
+        <h1>Hello brow !</h1>
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
